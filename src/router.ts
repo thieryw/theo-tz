1525b7f392d9102050c000a09c399f9166243b2e
Joseph Garrone <joseph.garrone.gj@gmail.com>
2020-11-28 01:05:58 +0100

    try

--- src/router.ts
+++ src/router.ts
@@ -1,41 +1,26 @@
-import {createRouter, defineRoute, RouterOpts} from "type-route";
-
-
-
-
-const opts: RouterOpts = {
-    scrollToTop: false,
-    
-}
-
-
+import { createRouter, defineRoute } from "type-route";
+import { makeThisModuleAnExecutableRouteLister } from "github-pages-plugin-for-type-route";
 
 const root = defineRoute(process.env["PUBLIC_URL"]);
-
 const naturalism = root.extend("/naturalisme");
 
-
-export const {RouteProvider, useRoute, routes} = createRouter(opts,{
-
-
-    home: root,
-
-    author: root.extend("/auteur"),
-
-    portraits: root.extend("/portraits"),
-
+export const routeDefs = {
+    "home": root,
+    "author": root.extend("/auteur"),
+    "portraits": root.extend("/portraits"),
     naturalism,
-    antilles: naturalism.extend("/antilles"),
-    reunion: naturalism.extend("/reunion"),
-    canada: naturalism.extend("/ouest-canadien"),
-    france: naturalism.extend("/france"),
+    "antilles": naturalism.extend("/antilles"),
+    "reunion": naturalism.extend("/reunion"),
+    "canada": naturalism.extend("/ouest-canadien"),
+    "france": naturalism.extend("/france"),
+    "events": root.extend("/evenements")
+};
 
+makeThisModuleAnExecutableRouteLister(routeDefs);
 
-
-    events: root.extend("/evenements")
-
-
-
-
-
-});
+export const { RouteProvider, useRoute, routes } = createRouter(
+    {
+        "scrollToTop": false
+    },
+    routeDefs
+);
