001dd4cc7d5e891fe76320bc01595a935aef30da
Joseph Garrone <joseph.garrone.gj@gmail.com>
2020-12-24 05:23:13 +0100

    Fix paths

--- src/bin/main.ts
+++ src/bin/main.ts
@@ -2,30 +2,42 @@
 import { crawl } from "./utils/crawl";
 
 import { arrayOfPathToDirectoryObject } from "./utils/arrayOfPathToDirectoryObject"
-import { directoryObjectOfParsedImagesDirectoryObject } from "./directoryObjectOfParsedImagesDirectoryObject";
-import { stringifyToSourceCodeParsedImageDirectoryObject } from "./stringifyToSourceCodeParsedImageDirectoryObject";
+import { directoryObjectOfParsedImagesDirectoryObject } from "./directoryObjectOfParsedImagesDirectoryObject";
+import { stringifyToSourceCodeParsedImageDirectoryObject } from "./stringifyToSourceCodeParsedImageDirectoryObject";
 import * as fs from "fs";
-import { join as pathJoin } from "path";
+import { join as pathJoin, relative as pathRelative } from "path";
 
-const directoryPath = pathJoin("src", "assets", "gallery");
+const srcDirectoryPath = "src";
 
-const arrayOfPath = crawl(directoryPath);
+const galleryDirectoryPath = pathJoin(srcDirectoryPath, "assets", "gallery");
+
+const arrayOfPath = crawl(galleryDirectoryPath)
+    .map(pathRelativeToGalleryDirectory => pathJoin(
+        pathRelative(srcDirectoryPath, galleryDirectoryPath),
+        pathRelativeToGalleryDirectory
+    ));
 
 const directoryObject = arrayOfPathToDirectoryObject({ arrayOfPath });
 
-const { 
-    parsedImagesDirectoryObject, 
-    imagesImportStatements 
+const {
+    parsedImagesDirectoryObject,
+    imagesImportStatements
 } = directoryObjectOfParsedImagesDirectoryObject({ directoryObject });
 
-const { sourceCode } = stringifyToSourceCodeParsedImageDirectoryObject({ 
-    parsedImagesDirectoryObject, 
-    imagesImportStatements 
+const { sourceCode } = stringifyToSourceCodeParsedImageDirectoryObject({
+    parsedImagesDirectoryObject,
+    imagesImportStatements
 });
 
 fs.writeFileSync(
     pathJoin("src", "generatedGalleryAsset.ts"),
-    Buffer.from(sourceCode, "utf8")
+    Buffer.from(
+        [
+        `// This file have been automatically generated do not edit manually`,
+        sourceCode, 
+        ].join("\n"),
+        "utf8"
+    )
 );
 
 
